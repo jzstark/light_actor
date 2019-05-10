647d31301166b43a9e9b5d5816c98e1484bb6285
jzstark <rho.ajax@gmail.com>
2019-05-10 11:17:00 +0100

    update barrier impl.

--- src/core/actor_barrier_ssp.ml
+++ src/core/actor_barrier_ssp.ml
@@ -11,41 +11,31 @@ let sample nodes_kv p =
   let idx = Actor_param_utils.lotto_select p (n - 1) in
   Array.map (Array.get nodes_kv) idx
 
+(* let sample = Owl_stats.choose --> does not work either; problem not on impl *)
 
-(* module Make (P : Actor_barrier_sig.Param) = struct *)
+let pass s p book =
+  let nodes_kv = Actor_param_utils.htbl_to_arr book in
+  (* to simplify compuation, all nodes uses one common sample *)
+  let nodes_kv_sample = match p with
+  | Some p -> sample nodes_kv p
+  | None   -> nodes_kv
+  in
+  let slowest = Array.fold_left (fun acc (_, node) ->
+    min node.step acc
+  ) max_int nodes_kv_sample
+  in
 
-  let pass s p book =
-
-    let nodes_kv = Actor_param_utils.htbl_to_arr book in
-    let nodes_kv = match p with
-    | Some p -> sample nodes_kv p
-    | None   -> nodes_kv
-    in
-
-    let fastest = Array.fold_left (fun acc (_, node) ->
-      max node.step acc
-    ) min_int nodes_kv
-    in
-
-    let slowest = Array.fold_left (fun acc (_, node) ->
-      min node.step acc
-    ) max_int nodes_kv
-    in
-
-    let passed = ref [||] in
-    Array.iter (fun (uuid, node) ->
-      if (fastest - node.step <= s && node.step - slowest <= s
-        && node.busy = false) then (
-        node.busy <- true;
-        passed := Array.append !passed [| uuid |]
-      )
-    ) nodes_kv;
-    !passed
+  let passed = ref [||] in
+  Array.iter (fun (uuid, node) ->
+    if (node.busy = false && (node.step - slowest <= s)) then (
+      node.busy <- true;
+      passed := Array.append !passed [| uuid |]
+    )
+  ) nodes_kv;
+  !passed
 
 
-  let sync book uuid =
-    let step = Actor_book.get_step book uuid in
-    Actor_book.set_busy book uuid false;
-    Actor_book.set_step book uuid (step + 1)
-
-(* end *)
+let sync book uuid =
+  let step = Actor_book.get_step book uuid in
+  Actor_book.set_busy book uuid false;
+  Actor_book.set_step book uuid (step + 1)
